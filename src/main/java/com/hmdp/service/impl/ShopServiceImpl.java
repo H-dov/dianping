b5830ebfde9cad70564c2b047c0e0753e6021fe4
David <1837707136@qq.com>
2025-05-09 16:51:30 +0800

    null缓存解决缓存穿透、互斥锁解决缓存击穿、逻辑过期解决缓存击穿

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -8,11 +10,15 @@ import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
@@ -30,20 +36,138 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Override
     public Result queryShopById(Long id) {
-        String key = RedisConstants.CACHE_SHOP_KEY + id;
-        String json = redisTemplate.opsForValue().get(key);
-        if(StrUtil.isNotBlank(json)){
-            Shop shop = JSONUtil.toBean(json, Shop.class);
-            return Result.ok(shop);
-        }
-        Shop shop = getById(id);
-        if(shop == null){
+        //缓存穿透
+        //Shop shop = queryWithPassThrough(id);
+        //互斥锁解决缓存击穿
+        //Shop shop = queryWithMutex(id);
+        //逻辑过期解决缓存击穿
+        Shop shop = queryWithLogicalExpire(id);
+        if (shop == null) {
             return Result.fail("店铺不存在");
         }
-        redisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
     }
 
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    /**
+     * 逻辑过期解决缓存击穿
+     *
+     * @param id
+     * @return
+     */
+    public Shop queryWithLogicalExpire(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String json = redisTemplate.opsForValue().get(key);
+        if(StrUtil.isBlank(json)){
+            return null;
+        }
+        RedisData redisData = JSONUtil.toBean(json, RedisData.class);
+        Shop shop = JSONUtil.toBean((JSONObject) redisData.getData(), Shop.class);
+        if (redisData.getExpireTime().isAfter(LocalDateTime.now())) {
+            return shop;
+        }
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        boolean isLock = tryLock(lockKey);
+        if(isLock){
+            CACHE_REBUILD_EXECUTOR.submit(() -> {
+                try {
+                    saveShop2Redis(id, 20L);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    unlock(lockKey);
+                }
+            });
+        }
+        return shop;
+    }
+
+    /**
+     * 互斥锁解决缓存击穿
+     *
+     * @param id
+     * @return
+     */
+    public Shop queryWithMutex(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String json = redisTemplate.opsForValue().get(key);
+        if(StrUtil.isNotBlank(json)){
+            return JSONUtil.toBean(json, Shop.class);
+        }
+        if(json != null){
+            return null;
+        }
+
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        Shop shop = null;
+        try {
+            boolean isLock = tryLock(lockKey);
+            if(!isLock){
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+            json = redisTemplate.opsForValue().get(key);
+            if(StrUtil.isNotBlank(json)){
+                return JSONUtil.toBean(json, Shop.class);
+            }
+
+            shop = getById(id);
+            if(shop == null){
+                redisTemplate.opsForValue().set(key,"", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+            redisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            unlock(lockKey);
+        }
+        return shop;
+    }
+
+    /**
+     * 缓存穿透
+     *
+     * @param id
+     * @return
+     */
+    public Shop queryWithPassThrough(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String json = redisTemplate.opsForValue().get(key);
+        if(StrUtil.isNotBlank(json)){
+            return JSONUtil.toBean(json, Shop.class);
+        }
+        if(json != null){
+            return null;
+        }
+        Shop shop = getById(id);
+        if(shop == null){
+            redisTemplate.opsForValue().set(key,"", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return null;
+        }
+        redisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        return shop;
+    }
+
+    public boolean tryLock(String key){
+        Boolean flag = redisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    public void unlock(String key){
+        redisTemplate.delete(key);
+    }
+
+    public void saveShop2Redis(Long id, Long expireSeconds) throws InterruptedException {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        Shop shop = getById(id);
+        Thread.sleep(200);
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expireSeconds));
+        redisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
+    }
     /**
      * 更新商铺信息
      *
